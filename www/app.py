e3485fc337153d87b6690ff3bbb60b5a35585707
simonjoe246 <simonjoe246@gmail.com>
2018-03-12 18:05:58 +0800

    day8 complete

--- www/app.py
+++ www/app.py
@@ -106,11 +106,11 @@ def datetime_filter(t):
     if delta < 60:
         return u'1分钟前'
     if delta < 3600:
-        return u'1小时前'
+        return u'%s分钟前' %(delta//60)
     if delta < 86400:
-        return u'1天前'
+        return u'%s小时前' %(delta//3600)
     if delta < 604800:
-        return u'%天前' % (delta//86400)
+        return u'%s天前' % (delta//86400)
     dt = datetime.fromtimestamp(t)
     return u'%年%月%日' % (dt.year, dt.month, dt.day)
 
